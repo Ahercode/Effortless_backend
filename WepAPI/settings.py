6c1d3adf9ef4f5b1cfa4f673c143e150a9aebe0c
unknown <Administrator@VPS.ksedalo.com>
2025-06-10 12:47:43 +0000

    clean up for settings

--- WepAPI/settings.py
+++ WepAPI/settings.py
@@ -107,10 +107,7 @@ WSGI_APPLICATION = 'WepAPI.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME'),
